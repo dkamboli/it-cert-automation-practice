d645796fcee2d345a5aa1f42da999d8b4259496a
Tania <gmailaccount@gmail.com>
2024-05-17 07:44:00 +0000

    Closes: #1
    Updated validations.py python script.
    
    Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,23 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Username can't begit with a dot
+    if username[0]== '.' or username[0]=='_':
+        return False 
     return True
 
 
 
+print(validate_user("blue.kale", 3)) # True
+
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+
+print(validate_user("red_quinoa", 4)) # True
+
+print(validate_user("_red_quinoa", 4)) # Currently True, should be Falseprint(validate_user("blue.kale", 3)) # True
+
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+
+print(validate_user("red_quinoa", 4)) # True
+
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
